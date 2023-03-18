3b67b65b42f71787f9b787ddcb86cb0c53359190
Tomasz Kierzenkowski <107223633+zivavu@users.noreply.github.com>
2023-03-18 13:01:40 +0100

    feat: Fetch data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react';
 import './styles.css';
 
 function App() {
+  const [carts, setCarts] = useState();
+  useEffect(() => {
+    const fetchCarts = async () => {
+      const data = await fetch('https://dummyjson.com/carts');
+      const json = await data.json();
+      setCarts(json.carts);
+    };
+
+    fetchCarts().catch(console.error);
+  }, []);
+
   return <div></div>;
 }
 
