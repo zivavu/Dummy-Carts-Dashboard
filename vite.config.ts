3b2c9ce6fb93b462fcb5f33cd1450335e498894b
Tomasz Kierzenkowski <107223633+zivavu@users.noreply.github.com>
2023-03-23 17:35:40 +0100

    ci: Update vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Dummy-Carts-Dashboard/',
   build: {
     outDir: 'dist',
   },
